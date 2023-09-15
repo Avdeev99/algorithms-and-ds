3331eb4209a76669ab91ef7c51008006db613212
Oleksii_Avdieiev <avdieiev.oleksii@gmail.com>
2023-09-15 09:49:27 +0400

    Added easy arrays tasks solution

---
+++ AlgorithmsAndDS/AlgorithmsAndDS/ArraysAndHashing/Easy/FindAllNumbersDisappearedInArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1.ArraysAndHashing.Easy;
+
+public static class FindAllNumbersDisappearedInArray
+{
+    // Time complexity: O(n); Space complexity: O(1).
+    public static IList<int> FindDisappearedNumbers(int[] nums)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var index = Math.Abs(nums[i]) - 1;
+            nums[index] = -Math.Abs(nums[index]);
+        }
+
+        var result = new List<int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > 0)
+                result.Add(i+1);
+        }
+
+        return result;
+    }
+}
---
+++ AlgorithmsAndDS/AlgorithmsAndDS/ArraysAndHashing/Easy/RangeSumQueryImmutable.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1.ArraysAndHashing.Easy;
+
+public class RangeSumQueryImmutable
+{
+    private readonly int[] _prefixSums;
+
+    // Time complexity: O(n); Space complexity: O(n).
+    public RangeSumQueryImmutable(int[] nums)
+    {
+        _prefixSums = new int[nums.Length];
+        var sum = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            _prefixSums[i] = sum;
+        }
+    }
+    
+    // Time complexity: O(1); Space complexity: O(1).
+    public int SumRange(int left, int right)
+    {
+        var rightVal = _prefixSums[right];
+        var leftVal = left > 0 ? _prefixSums[left - 1] : 0;
+
+        return rightVal - leftVal;
+    }
+}
