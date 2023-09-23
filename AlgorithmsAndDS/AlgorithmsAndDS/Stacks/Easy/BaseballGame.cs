4acd3c57d89a15e2649cb1f4286a521a8e98a58b
Oleksii_Avdieiev <avdieiev.oleksii@gmail.com>
2023-09-23 14:51:17 +0400

    Added easy stack problem solution

---
+++ AlgorithmsAndDS/AlgorithmsAndDS/Stacks/Easy/BaseballGame.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1.Stacks.Easy;
+
+public class BaseballGame
+{
+    // Time complexity: O(n); Space complexity: O(n).
+    public int CalPoints(string[] operations)
+    {
+        var stack = new Stack<int>();
+        var result = 0;
+
+        foreach (var operation in operations)
+        {
+            var record = 0;
+
+            if (int.TryParse(operation, out record))
+            {
+                stack.Push(record);
+            }
+            else if (operation == "+")
+            {
+                var last = stack.Pop();
+                record = last + stack.Peek();
+                stack.Push(last);
+                stack.Push(record);
+            }
+            else if (operation == "D")
+            {
+                record = stack.Peek() * 2;
+                stack.Push(record);
+            }
+            else if (operation == "C")
+            {
+                var last = stack.Pop();
+                result -= last;
+            }
+
+            result += record;
+        } 
+
+        return result;   
+    }
+}
