4533edef19e1e4f60ff04302b899018f47d5ad34
Oleksii_Avdieiev <avdieiev.oleksii@gmail.com>
2023-09-23 14:51:00 +0400

    Added easy sliding window problem solution

---
+++ AlgorithmsAndDS/AlgorithmsAndDS/SlidingWindow/Easy/ContainsDuplicateII.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1.SlidingWindow.Easy;
+
+public class ContainsDuplicateII
+{
+    // Time complexity: O(n); Space complexity: O(n).
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        var dict = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (dict.ContainsKey(nums[i]) && i - dict[nums[i]] <= k)
+                return true;
+
+            dict[nums[i]] = i;
+        }
+
+        return false;
+    }
+}
