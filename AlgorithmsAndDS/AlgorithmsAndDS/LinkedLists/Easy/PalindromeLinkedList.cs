425cd1dd8c03cafde4ef869b8221144565513c7f
Oleksii_Avdieiev <avdieiev.oleksii@gmail.com>
2023-09-25 22:02:15 +0400

    Added easy linked lists problems solutions

---
+++ AlgorithmsAndDS/AlgorithmsAndDS/LinkedLists/Easy/PalindromeLinkedList.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleApp1.Helpers;
+
+namespace ConsoleApp1.LinkedLists.Easy;
+
+public class PalindromeLinkedList
+{
+    // Time complexity: O(n); Space complexity: O(1).
+    public bool IsPalindrome(ListNode head)
+    {
+        var slow = head;
+        var fast = head;
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        var previous = slow;
+        slow = slow.next;
+        previous.next = null;
+
+        // reverse second part of the list
+        while (slow != null)
+        {
+            var temp = slow.next;
+            slow.next = previous;
+            previous = slow;
+            slow = temp;
+        }
+
+        fast = head;
+        slow = previous;
+
+        while (slow != null)
+        {
+            if (slow.val != fast.val) return false;
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return true;
+    }
+}
---
+++ AlgorithmsAndDS/AlgorithmsAndDS/LinkedLists/Easy/RemoveLinkedListElements.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp1.BlindSpot75;
+
+namespace ConsoleApp1.LinkedLists.Easy;
+
+public class RemoveLinkedListElements
+{
+    // Time complexity: O(n); Space complexity: O(1).
+    public ListNode RemoveElements(ListNode head, int val)
+    {
+        var dummy = new ListNode(next: head);
+
+        var prev = dummy;
+        var curr = head;
+
+        while (curr != null)
+        {
+            if (curr.val == val)
+                prev.next = curr.next;
+            else
+                prev = curr;
+            
+            curr = curr.next;
+        }
+
+        return dummy.next;  
+    }
+}
