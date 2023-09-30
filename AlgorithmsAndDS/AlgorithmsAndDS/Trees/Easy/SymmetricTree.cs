e83a043386ad731e3f19e576bea42a90a6bec478
Oleksii_Avdieiev <avdieiev.oleksii@gmail.com>
2023-09-30 15:42:14 +0400

    Added solution to symmetric tree problem

---
+++ AlgorithmsAndDS/AlgorithmsAndDS/Trees/Easy/SymmetricTree.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using ConsoleApp1.Helpers;
+
+namespace ConsoleApp1.Trees.Easy;
+
+public class SymmetricTree
+{
+    public bool IsSymmetric(TreeNode root)
+    {
+        if (root == null) return true;
+
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root.left);
+        queue.Enqueue(root.right);
+
+        while(queue.Count > 0)
+        {
+            var left = queue.Dequeue();
+            var right = queue.Dequeue();
+
+            if(left == null && right == null)
+                continue;
+
+            if(left == null || right == null || left.val != right.val )
+                return false;
+
+            queue.Enqueue(left.left);
+            queue.Enqueue(right.right);
+            queue.Enqueue(left.right);
+            queue.Enqueue(right.left);
+        }
+
+        return true;
+    }
+}
