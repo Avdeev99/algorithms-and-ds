65d134a765c735f1fb62f81ebeb8d6d9cd336c7b
Oleksii_Avdieiev <avdieiev.oleksii@gmail.com>
2023-10-07 12:04:06 +0400

    Added medium array problems solutions

---
+++ AlgorithmsAndDS/AlgorithmsAndDS/ArraysAndHashing/Medium/LargestNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1.ArraysAndHashing.Medium;
+
+public class LargestNumber
+{
+    // Time complexity: O(nlog(n)); Space complexity: O(n).
+    public string GetLargestNumber(int[] nums)
+    {
+        var numStrs = nums
+            .Select(num => num.ToString())
+            .ToArray();
+
+        Array.Sort(numStrs, (num1, num2) =>
+        {
+            var opt1 = num1 + num2;
+            var opt2 = num2 + num1;
+
+            return opt2.CompareTo(opt1);
+        });
+
+        var result = string.Join("", numStrs);
+
+        if (result[0] == '0' && result.Length > 0)
+            return "0";
+
+        return result;
+    }
+}
---
+++ AlgorithmsAndDS/AlgorithmsAndDS/ArraysAndHashing/Medium/SortColors.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1.ArraysAndHashing.Medium;
+
+public class SortColors
+{
+    // Time complexity: O(n); Space complexity: O(1).
+    public void Sort(int[] nums)
+    {
+        var zeroIndex = 0;
+        var twoIndex = nums.Length- 1;
+
+        var i = 0;
+
+        while (i <= twoIndex)
+        {
+            if (nums[i] == 0)
+            {
+                (nums[i], nums[zeroIndex]) = (nums[zeroIndex], nums[i]);
+                zeroIndex++;
+            }
+            else if (nums[i] == 2)
+            {
+                (nums[i], nums[twoIndex]) = (nums[twoIndex], nums[i]);
+                twoIndex--;
+                i--;
+            }
+
+            i++;
+        }
+    }
+}
