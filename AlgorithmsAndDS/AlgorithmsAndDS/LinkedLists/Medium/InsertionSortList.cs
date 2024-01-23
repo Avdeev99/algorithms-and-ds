107d8d19b7da609c654cca1d9f0b521934169dee
oleksii_avdieiev <avdieiev.oleksii@gmail.com>
2024-01-23 12:11:22 +0200

    Added medium problem solutions

---
+++ AlgorithmsAndDS/AlgorithmsAndDS/LinkedLists/Medium/InsertionSortList.cs
@@ -0,0 +1,40 @@
+using ConsoleApp1.Helpers;
+
+namespace ConsoleApp1.LinkedLists.Medium;
+
+// 147. Insertion Sort List
+public class InsertionSortList
+{
+    // Time complexity: O(n^2); Space complexity: O(1)
+    public ListNode InsertionSort(ListNode head)
+    {
+        var dummy = new ListNode(0, head);
+
+        var curr = head.next;
+        var prev = head;
+
+        while (curr != null)
+        {
+            if (curr.val > prev.val)
+            {
+                prev = curr;
+                curr = curr.next;
+                continue;
+            }
+
+            var pointer = dummy;
+
+            while (pointer.next != null && curr.val > pointer.next.val)
+            {
+                pointer = pointer.next;
+            }
+
+            prev.next = curr.next;
+            curr.next = pointer.next;
+            pointer.next = curr;
+            curr = prev.next;
+        }
+
+        return dummy.next;  
+    }
+}
---
+++ AlgorithmsAndDS/AlgorithmsAndDS/LinkedLists/Medium/SplitLinkedListInParts.cs
@@ -0,0 +1,95 @@
+using ConsoleApp1.Helpers;
+
+namespace ConsoleApp1.LinkedLists.Medium;
+
+// 725. Split Linked List in Parts
+public class SplitLinkedListInParts
+{
+    // Time complexity: O(n); Space complexity: O(n).
+    public ListNode[] SplitListToParts(ListNode head, int k)
+    {
+        var length = 0;
+        var curr = head;
+
+        while (curr != null)
+        {
+            length++;
+            curr = curr.next;
+        }
+
+        var partSize = length / k;
+        var mod = length % k;
+        var result = new ListNode[k];
+        curr = head;
+
+        for (var i = 0; i < k; i++)
+        {
+            result[i] = curr;
+            
+            var size = mod > 0
+                ? partSize + 1
+                : partSize;
+
+            for (var j = 0; j < size - 1 && curr != null; j++)
+            {
+                curr = curr.next;
+            }
+
+            mod--;
+
+            if (curr == null) continue;
+
+            var temp = curr.next;
+            curr.next = null;
+            curr = temp;
+        }
+
+        return result;
+    }
+
+    // Time complexity: O(n); Space complexity: O(n).
+    public ListNode[] SplitListToPartsRecursive(ListNode head, int k)
+    {
+        var length = 0;
+        var curr = head;
+
+        while (curr != null)
+        {
+            length++;
+            curr = curr.next;
+        }
+
+        var partSize = length / k;
+        var mod = length % k;
+
+        var result = new ListNode[k];
+        Helper(head, partSize, mod, 0, result);
+
+        return result;
+    }
+
+    private void Helper(ListNode head, int size, int mod, int index, ListNode[] result)
+    {
+        if (head == null) return;
+        
+        ListNode prev = null;
+        var curr = head;
+
+        for (var i = 0; i < size; i++)
+        {
+            prev = curr;
+            curr = curr.next;
+        }
+
+        if (curr != null && mod > 0)
+        {
+            prev = curr;
+            curr = curr.next;
+            mod--;
+        }
+
+        prev.next = null;
+        result[index++] = head;
+        Helper(curr, size, mod, index, result);
+    }
+}
